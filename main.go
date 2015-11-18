8d3c9d572e9dd032aca76fa848ab054d258d0f33
Mark Haferkamp <github.com@refola.com>
2015-11-18 15:57:11 -0700

    was meant to be moved instead of copied...

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/refola/piklisp_go/internal/parse"
-
-func main() {
-	test_program := "(import \"fmt\")\n(fmt.Println \"Hello world!\")"
-	parsed := parse.ParenString(test_program)
-	fmt.Println(parsed)
-}
