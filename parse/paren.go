e1bc202dc38b7a535fd767e932fe3f7b74ed18d6
Mark Haferkamp <github.com@refola.com>
2016-01-14 14:06:45 -0700

    replace failed inline with findCloseQuote()

--- parse/paren.go
+++ parse/paren.go
@@ -4,14 +4,13 @@ package parse
 
 import (
 	"errors"
-	"strings"
 )
 
 // Given a string containing a backslash-escaped "string" and an opening quote's index, find the closing quote's index. Returns a negative value if there is no closing quote.
 func findCloseQuote(s string, open int) int {
 	for i := open + 1; i < len(s); i++ {
 		switch s[i] {
-		case s[open]: 
+		case s[open]:
 			return i
 		case '\\':
 			i++ // We don't care what the escaped character is, so we just skip it.
@@ -31,20 +30,11 @@ func ParenString(s string) (*Tree, error) {
 	for s != "" {
 		switch s[0] {
 		case '"': // search for next unescaped double quote
-			skipped := 1 // how many characters we know must be skipped before the next unescaped double quote
-			foundMatch := false
-			for !foundMatch {
-				i := strings.Index(s[skipped:], "\"")
-				if i < 0 {
-					return err("unmatched \"")
-				}
-				if i > 0 && s[i-1] == '\\' { // skip quoted match
-					skipped += i + 1
-				} else {
-					child.content = s[:i]
-				}
+			end := findCloseQuote(s, 0)
+			if end < 0 {
+				return err("missing closing quote")
+			} else {
 				// coi la .sampla. .i do ca zvati: need to keep track of where we are in the parse -- maybe turn Tree into Node? This 'else' should append the string to the current node's list of contents.
-
 			}
 		case '(': // start group and search for next unquoted close-paren
 		case ' ', '\n', '\t': // start making next group element at current level
