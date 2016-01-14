7a36e03ce680e452dd667ee9d06aa00b18feb721
Mark Haferkamp <github.com@refola.com>
2016-01-14 13:56:10 -0700

    added function for finding closing quote

--- parse/paren.go
+++ parse/paren.go
@@ -7,6 +7,21 @@ import (
 	"strings"
 )
 
+// Given a string containing a backslash-escaped "string" and an opening quote's index, find the closing quote's index. Returns a negative value if there is no closing quote.
+func findCloseQuote(s string, open int) int {
+	for i := open + 1; i < len(s); i++ {
+		switch s[i] {
+		case s[open]: 
+			return i
+		case '\\':
+			i++ // We don't care what the escaped character is, so we just skip it.
+		default:
+			continue
+		}
+	}
+	return -1
+}
+
 // parse a string of parenthesis-grouped code into a tree
 func ParenString(s string) (*Tree, error) {
 	err := func(s string) (*Tree, error) {
