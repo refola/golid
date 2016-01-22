ea24f981cee6aa2a7b70ec232166043f4c7e7f40
Mark Haferkamp <github.com@refola.com>
2016-01-21 23:14:33 -0700

    Fix "Hello world!" program in non-running language

--- piklisp/main.go
+++ piklisp/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/refola/piklisp_go/parse"
 
 func main() {
-	test_program := "(import \"fmt\")\n(fmt.Println \"Hello world!\")"
+	test_program := "(package main)\n(import \"fmt\")\n(func main () () (fmt.Println \"Hello piklisp_go!\"))"
 	parsed := parse.ParenString(test_program)
 	fmt.Println(parsed)
 }
