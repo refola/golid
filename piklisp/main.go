2e0292b3a1688f61626c35ac82b66eb2e89d3dbd
Mark Haferkamp <github.com@refola.com>
2016-01-21 23:21:51 -0700

    nice formatting for test program

--- piklisp/main.go
+++ piklisp/main.go
@@ -2,8 +2,13 @@ package main
 
 import "github.com/refola/piklisp_go/parse"
 
+const test_program = // This comment saves formatting from being eaten by gofmt.
+`(package main)
+(import "fmt")
+(func main () ()
+	(fmt.Println "Hello piklisp_go!"))`
+
 func main() {
-	test_program := "(package main)\n(import \"fmt\")\n(func main () () (fmt.Println \"Hello piklisp_go!\"))"
 	parsed := parse.ParenString(test_program)
 	fmt.Println(parsed)
 }
