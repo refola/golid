5fd907c02105ee07bd5256092bbcaa65bf08a52b
Mark Haferkamp <github.com@refola.com>
2016-02-10 22:53:33 -0700

    Piklisp code is in its own file!

--- piklisp/main.go
+++ piklisp/main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/refola/piklisp_go/parse"
 )
 
-const test_program = // This comment saves formatting from being eaten by gofmt.
-`(package main)
-(import "fmt")
-(func main () ()
-	(fmt.Println "Hello piklisp_go!"))`
+const usage = `Usage: piklisp file1.plgo [file2.plgo [...]]
+
+Converts Piklisp Go code into Go.`
+
+func e(err error) {
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		os.Exit(1)
+	}
+}
 
 func main() {
-	parsed, _ := parse.ParenString(test_program)
-	fmt.Println("Lisp code loaded!")
-	fmt.Println("\nHere's the Lisp version, parsed and unparsed.")
-	fmt.Println(parsed)
-	fmt.Println("\nHere's the automatic ('compiled') Go translation.")
-	fmt.Println(parsed.GoString())
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+	for _, input_name := range args {
+		if input_name[len(input_name)-len(".plgo"):] != ".plgo" {
+			e(errors.New("Invalid filename: " + "input_name"))
+		}
+		output_name := input_name[:len(input_name)-len(".plgo")] + ".go"
+		lisp_bytes, err := ioutil.ReadFile(input_name)
+		lisp_text := string(lisp_bytes)
+		e(err)
+		lisp_parse, err := parse.ParenString(lisp_text)
+		e(err)
+		go_text := lisp_parse.GoString()
+		err = ioutil.WriteFile(output_name, []byte(go_text), os.ModePerm)
+		e(err)
+	}
 }
