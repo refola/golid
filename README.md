d3d1ee57b62769a1bca59104b792f77ac9805107
Mark Haferkamp <github.com@refola.com>
2016-02-12 01:19:07 -0700

    refactor file conversion

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ Here's how to use this as-is:
 * Checkout this repository with Git.
 * Open a console in the directory you downloaded this to.
 * Run `go build ./piklisp/main.go`.
-* Run `./main file.{plgo|gol}` to convert `file.{plgo|gol}` into `file_{plgo|gol}.go`.
+* Run `./main file.{plgo|gol}` to convert `file.{plgo|gol}` into `{plgo|gol}_file.go`.
 * Run `go test ./parse` to run current tests.
 
-Try running `./main tests/interesting_looking_file.gol` followed by `go run tests/interesting_looking_file_gol.go` on whichever file looks interesting to see what runs with this so far.
+Try running `./main tests/interesting_looking_file.gol` followed by `go run tests/gol_interesting_looking_file.go` on whichever file looks interesting to see what runs with this so far.
 
 License:
 This is licensed as GPLv3 because that's the most restrictive license GitHub offers by default. I know that this is an inappropriate license for something resembling a programming language. If for some reason you want to use experimental learning code for your GPLv3-incompatible project, then please make a bug report describing your project and why you want to use piklisp.go for it in a GPLv3-incompatible way. I'll gladly change the license in exchange for having a public record of someone wanting to use my project. Depending on demand, I'm potentially willing to go as far as public domain or the "Unlicense". But I need a good reason to relicense.
--- parse/convert.go
+++ parse/convert.go
@@ -6,38 +6,50 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 )
 
-const plgo = ".plgo" // praenthesized version of Piklisp-Go
-const gol = ".gol"   // less parens extension
+const plgo = "plgo" // praenthesized version of Piklisp-Go
+const gol = "gol"   // less parens extension
+
+// split filename into base and extension, returning original name if no '.' found
+func split_filename(fn string) (string, string) {
+	for i := len(fn) - 1; i >= 0; i-- {
+		if fn[i] == '.' {
+			return fn[:i-1], fn[i+1:]
+		}
+	}
+	return fn, ""
+}
+
+// Read a file into a Piklisp syntax tree
+func ReadPiklisp(plfile string) (Expression, error) {
+	_, ext := split_filename(plfile)
+	parseFn := ParenString
+	switch ext {
+	case plgo: // valid case, but we already have its parseFn set
+	case gol:
+		parseFn = Srfi49String
+	default:
+		return nil, fmt.Errorf("File %s has non-Piklisp extension %s", plfile, ext)
+	}
+	lisp_bytes, err := ioutil.ReadFile(plfile)
+	if err != nil {
+		return nil, err
+	}
+	lisp_text := string(lisp_bytes)
+	return parseFn(lisp_text)
+}
 
 // Convert a Piklisp file into Go. It uses normal Lisp syntax if the extension is .plgo and SRFI#49 if the extension is .gol.
 func Convert(plfile string, write_result bool) error {
-	ext := ""
-	parseFn := ParenString
-	switch {
-	case strings.HasSuffix(plfile, plgo):
-		ext = plgo
-	case strings.HasSuffix(plfile, gol):
-		ext = gol
-		parseFn = Srfi49String
-	default:
-		return fmt.Errorf("Invalid filename: %s", plfile)
-	}
-
-	lisp_bytes, err := ioutil.ReadFile(plfile)
+	parsed, err := ReadPiklisp(plfile)
 	if err != nil {
 		return err
 	}
-	lisp_text := string(lisp_bytes)
-	lisp_parse, err := parseFn(lisp_text)
-	if err != nil {
-		return err
-	}
-	go_text := lisp_parse.GoString()
+	go_text := parsed.GoString()
+	name, ext := split_filename(plfile)
 	if write_result {
-		gofile := fmt.Sprintf("%s_%s.go", plfile[:len(plfile)-len(ext)], ext[1:])
+		gofile := fmt.Sprintf("%s_%s.go", ext, name)
 		err = ioutil.WriteFile(gofile, []byte(go_text), os.ModePerm)
 	}
 	return err
