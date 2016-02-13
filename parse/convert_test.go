057b591be2456ff88f73390dc4f30607bff70b70
Mark Haferkamp <github.com@refola.com>
2016-02-13 00:19:09 -0700

    continue after a test panics and stack traces

--- parse/convert_test.go
+++ parse/convert_test.go
@@ -3,25 +3,44 @@
 package parse
 
 import (
-	"testing"
-	//"os"
 	"io/ioutil"
+	"runtime"
+	"testing"
 )
 
 // check that each Piklisp file converts successfully to Go, without
 // crashing
 func TestConversions(t *testing.T) {
+	// Wrapper function to avoid panics stopping the test
+	parsable := func(fn string) (ret bool) {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("Error processing %s:\n%s\n", fn, r)
+				trace := make([]byte, 1e4)
+				i := runtime.Stack(trace, false)
+				t.Errorf("Stack trace:\n%s\n", trace[:i])
+			}
+		}()
+		err := Convert(fn, false)
+		if err != nil {
+			t.Errorf("Error processing %s:\n%s", fn, err)
+			return false
+		}
+		return true
+	}
+
 	prefix := "../tests"
 	files, err := ioutil.ReadDir(prefix)
 	if err != nil {
 		t.Fatal("Could not open tests folder:", err)
 	}
+	failcount := 0
 	for _, f := range files {
 		filename := prefix + "/" + f.Name()
-		t.Logf("Testing %s.\n", filename)
-		err := Convert(filename, false)
-		if err != nil {
-			t.Error(err)
+		if !parsable(filename) {
+			t.Errorf("Failed parsing %s.\n", filename)
+			failcount++
 		}
 	}
+	t.Logf("Failed to parse %d/%d test files.", failcount, len(files))
 }
