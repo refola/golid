67d3b95859c0a3fb2fb180e9eb9f3f8376bd65fa
Mark Haferkamp <github.com@refola.com>
2016-03-12 03:11:54 -0700

    reinstall command on test

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-echo "Formatting code."
+
+echo "Processing code."
+# gofmt everything
 find . -type f -name \*.go -print0 | xargs -0 -r -n1 gofmt -w
+# reinstall updated binary
+go install github.com/refola/piklisp_go/piklisp
 
 echo "Running tests."
 go test ./parse/
