0894a11925ae2dcafd8b65f8a315fcdc77623fec
Mark Haferkamp <github.com@refola.com>
2016-03-16 10:05:10 -0700

    start from script's location, not $PWD

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Run all the .plgo tests to make sure they do what they're meant to.
 
+# Make sure we're at the right place before doing stuff.
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
 cd ./tests/classic || exit 1
 echo "Running tests in $PWD."
 
--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Make sure we're at the script's location before doing stuff.
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
+
 echo "Processing code."
 # gofmt everything
 find . -type f -name \*.go -print0 | xargs -0 -r -n1 gofmt -w
