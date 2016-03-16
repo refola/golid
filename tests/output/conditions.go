4561089ae1abbfdd3aca261cd4ac82530376fded
Mark Haferkamp <github.com@refola.com>
2016-03-16 05:56:42 -0700

    tests for simplified "if"

---
+++ tests/output/conditions.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func foo() {
+	return true
+}
+
+func main() {
+	if true {
+		fmt.Println("true happens")
+	}
+	if false {
+		panic("false happened")
+	} else if true {
+		fmt.Println("even if it's after false")
+	}
+	if foo() {
+		fmt.Println("or if it's from a function")
+	}
+	if false {
+		panic("false happened")
+	} else if false {
+		panic("false happened")
+	} else if true {
+		fmt.Println("or after multiple falses")
+	}
+	if true {
+		fmt.Printf("or when it's on")
+		fmt.Println("multiple lines")
+	}
+}
