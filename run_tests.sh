7fcd8a667fcd845a494432c75ed9fe952fee80ca
Mark Haferkamp <github.com@refola.com>
2016-06-01 10:49:31 -0700

    somewhat improved output-checking script

--- run_tests.sh
+++ run_tests.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 # Run all the .gol tests to make sure they do what they're meant to.
 
+## Usage: clr color_code text
+# Changes text color according to given code, from 0 thru 7.
+clr() {
+    echo -e "\e[0;3${1}m$2\e[0m"
+}
+
 ## Usage: test-dir directory
 # Tests every .gol file in given directory
 test-dir() {
-    echo -e "\e[0;32mRunning tests in $1\e[0m"
+    clr 2 "Running tests in $1"
     cd "$1" || exit 1
+    local f
+    local out_file
+    local output
+    local expected
+    local show="failed"
+    if [ -f "./output.go" ]; then
+        expected="$(go run ./output.go)"
+    else
+        clr 2 "No output.go in $1. Showing raw output instead."
+        show="all"
+    fi
     for f in ./*; do
         if [ "$f" = "${f/.gol/}.gol" ]; then
             f="${f/.\//}" # strip leading ./
-            echo -e "\e[0;37mTesting $f.\e[0m"
-            out="gol_${f/.gol/}.go" # Golid → Go name conversion
+            out_file="gol_${f/.gol/}.go" # Golid → Go name conversion
             if golid "$f" 2>/dev/null; then # convert to Go
-                go run "$out" # run generated file if Golid worked
-                rm "$out" # and then clean up
+                # run generated file if Golid worked
+                if ! output="$(go run "$out_file" 2>/dev/null)"; then
+                    clr 1 "Error in $f: Could not run generated '$out_file'."
+                fi
+                rm "$out_file" # and then clean up
+                if [ "$show" = "all" ]; then
+                    clr 7 "$f:"
+                    echo "$output"
+                elif [ "$output" != "$expected" ]; then
+                    clr 1 "$f doesn't produce expected output."
+                    clr 1 "$f's results:"
+                    echo "$output"
+                    clr 1 "\nExpected output:"
+                    echo "$expected"
+                    echo
+                fi
             else
-                echo -e "\e[0;31mError! Golid failed on '$f'!\e[0m"
+                clr 1 "Error! Golid failed on '$f'!"
             fi
         fi
     done
