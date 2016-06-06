2518d5c269d3ac6a3fe481b4fb15e90add013b60
Mark Haferkamp <github.com@refola.com>
2016-06-06 11:41:17 -0700

    Add "-reparse" option to just print the Lisp

--- cmd/golid/main.go
+++ cmd/golid/main.go
@@ -7,9 +7,12 @@ import (
 	"github.com/refola/golid/parse"
 )
 
-const usage = `Usage: golid file1.gol [file2.gol [...]]
+const usage = `Usage: golid [-reparse] file1.gol [file2.gol [...]]
 
 Converts Golid code into Go, producing gol_file1.gol, etc.
+
+If the first argument is "-reparse", then print the Lisp parse tree to
+standart output instead.
 `
 
 func main() {
@@ -17,11 +20,22 @@ func main() {
 	if len(args) == 0 {
 		fmt.Println(usage)
 		os.Exit(1)
-	}
-	for _, file := range args {
-		err := parse.Convert(file)
-		if err != nil {
-			fmt.Printf("Error converting %s: %s\n", file, err)
+	} else if args[0] == "-reparse" {
+		for _, file := range args[1:] {
+			parsed, err := parse.ReadGolid(file)
+			if err != nil {
+				fmt.Printf("Error converting %s: %s\n", file, err)
+			}
+			reparsed := parsed.String()
+			reparsed = reparsed[1 : len(reparsed)-1] // remove wrapping "()"
+			fmt.Println(reparsed)
+		}
+	} else {
+		for _, file := range args {
+			err := parse.Convert(file)
+			if err != nil {
+				fmt.Printf("Error converting %s: %s\n", file, err)
+			}
 		}
 	}
 }
