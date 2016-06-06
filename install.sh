d8da8201992644e48b5c5bd601f568f1d4adf2d1
Mark Haferkamp <github.com@refola.com>
2016-06-06 11:35:09 -0700

    always gofmt and install before testing

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-echo "Installing latest Golid converter."
+# Make sure we're at the script's location before doing stuff.
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
+
+echo -n "Formatting code... "
+find . -type f -name \*.go -print0 | xargs -0 -r -n1 gofmt -w
+echo -n "Installing... "
 go install github.com/refola/golid/cmd/golid
+echo "Done!"
--- test.sh
+++ test.sh
@@ -3,9 +3,8 @@
 # Make sure we're at the script's location before doing stuff.
 cd "$(dirname "$(readlink -f "$0")")" || exit 1
 
-echo "Processing code."
-# gofmt everything
-find . -type f -name \*.go -print0 | xargs -0 -r -n1 gofmt -w
+# Get to the latest version.
+./install.sh
 
 echo "Running tests."
 go test ./parse/
